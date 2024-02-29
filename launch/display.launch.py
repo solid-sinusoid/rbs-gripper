2a46b9c44006ca57d0d007e76e0ebeaa733266bc
Bill Finger <ur.narmak@gmail.com>
2024-02-29 16:45:10 +0300

    rename prefix to tf_prefix

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -25,9 +25,9 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "prefix",
+            "tf_prefix",
             default_value='""',
-            description="Prefix of the joint names, useful for \
+            description="tf_prefix of the joint names, useful for \
         multi-robot setup. If changed than also joint names in the controllers' configuration \
         have to be updated.",
         )
