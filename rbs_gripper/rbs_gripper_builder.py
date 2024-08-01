3be7b938edf09c35fbac6e3e5860f6dba7f74632
Bill Finger <ur.narmak@gmail.com>
2024-08-01 20:05:07 +0300

    update lib to prepare web config folder

--- rbs_gripper/rbs_gripper_builder.py
+++ rbs_gripper/rbs_gripper_builder.py
@@ -6,13 +6,19 @@ from odio_urdf import *
 class RbsGrippperBuilder(RobotBuilderABC):
     def __init__(self,
                  robot_name:str, 
-                 parent:str) -> None:
+                 parent:str,
+                 save_path: str | None = None) -> None:
         self.robot_name = robot_name
         self.robot_type = "rbs_gripper"
+        self.save_path = save_path
         self.reset(robot_name, parent)
 
     def reset(self, robot_name, parent) -> None:
-        self._rc = RobotConfig(robot_name=robot_name, robot_type=self.robot_type, parent=parent)
+        self._rc = RobotConfig(robot_name=robot_name,
+                               robot_type=self.robot_type,
+                               parent=parent)
+
+        self.package_path = self._rc.robot_package_abs_path
 
     @property
     def robot(self) -> RobotConfig:
@@ -20,23 +26,23 @@ class RbsGrippperBuilder(RobotBuilderABC):
 
     def base(self) -> None:
         link0 = LinkNode(0, self.robot_name, 
-                         self._rc.robot_config["base"], "base", self._rc.robot_package_abs_path)
+                         self._rc.robot_config["base"], "base", self.package_path, self.save_path)
         joint0 = JointNode(0, self._rc.parent, link0.name, joint_config={"type": "fixed"})
         self._rc.add(joint0)
         self._rc.add(link0)
 
         link1 = LinkNode(1, self.robot_name, self._rc.robot_config["rot"]["link"],
-                         "rot", self._rc.robot_package_abs_path)
+                         "rot", self.package_path, self.save_path)
         self._rc.add(JointNode(1, link0.name, link1.name,
                                joint_config=self._rc.robot_config["rot"]["joint"]))
         self._rc.add(link1)
         link2 = LinkNode(2, self.robot_name, self._rc.robot_config["l_finger"]["link"],
-                         "l_finger", self._rc.robot_package_abs_path)
+                         "l_finger", self.package_path, self.save_path)
         self._rc.add(link2)
         self._rc.add(JointNode(2, link1.name, link2.name, 
                                joint_config=self._rc.robot_config["l_finger"]["joint"]))
         link3 = LinkNode(3, self.robot_name, self._rc.robot_config["r_finger"]["link"],
-                         "r_finger", self._rc.robot_package_abs_path)
+                         "r_finger", self.package_path, self.save_path)
         self._rc.add(link3)
         self._rc.add(JointNode(3, link1.name, link3.name,
                                joint_config=self._rc.robot_config["r_finger"]["joint"]))
@@ -44,7 +50,8 @@ class RbsGrippperBuilder(RobotBuilderABC):
         link_ee = LinkNode(None, self.robot_name,
                            link_config={},
                            link_name="grasp_point",
-                           package_path=self._rc.robot_package_abs_path)
+                           package_path=self.package_path,
+                           save_geometry_path=self.save_path)
         self._rc.add(JointNode(None, link1.name, link_ee.name, joint_config={
             "type": "fixed",
             "origin": [0,0, 0.09139, 0, 0, 0]
